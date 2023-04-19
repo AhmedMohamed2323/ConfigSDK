6f4f977a523ad83343455c8f704db15d68698c09
Ahmed Mohamed <ahmed.mohamed@circleci.com>
2023-04-19 16:38:58 -0400

    green build

--- .circleci/dynamic/index.js
+++ .circleci/dynamic/index.js
@@ -29,5 +29,5 @@ myWorkflow.addJob(nodeTestJob);
 // The `stringify()` function on `CircleCI.Config` will return the CircleCI YAML equivalent.
 const MyYamlConfig = myConfig.stringify();
 
-// Save the config to a file in Node.js or the browser. Note, use in the browser requires user interaction.
+// Save the config to a file in Node.js or the browser. Note, use in the browser requires user interaction.....
 myConfig.writeFile('dynamicConfig.yml');
